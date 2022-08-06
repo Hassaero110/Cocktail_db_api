d203adf8d9fed3f5f28bc1e5a132164fd7a967ed
Hassan Raza <hassan.raza3@nhs.net>
2022-08-06 11:18:32 +0100

    Added implementaion of get_drinks_from_ingredients() tests now pass

--- functions.py
+++ functions.py
@@ -1,2 +1,16 @@
 def get_drinks_from_ingredients(available_ingredients, drinks_database):
-    pass
+    """
+    Returns a list of drinks that can be made from the given ingredients.
+    """
+    drinks = []
+
+    for drink in drinks_database:
+
+        can_make_drink = True
+        for ingredient in drink['ingredients']:
+            if ingredient not in available_ingredients:
+                can_make_drink = False
+        if can_make_drink:
+            drinks.append(drink['name'])
+
+    return drinks
