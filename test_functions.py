29ef07b0d0b9e5f75bccdb18cd0a0002385ef532
Hassan Raza <hassan.raza3@nhs.net>
2022-08-06 12:40:36 +0100

    Added unit test for possible drinks and extended unit tests for get_drinks_from_ingredients()

--- test_functions.py
+++ test_functions.py
@@ -1,4 +1,6 @@
-from functions import get_drinks_from_ingredients
+import pytest
+from functions import get_drinks_from_ingredients, get_possible_drinks
+
 
 def mock_drinks_api():
     """
@@ -20,11 +22,54 @@ def mock_drinks_api():
     ]
 
 
-def test_get_drinks_from_ingredients():
+@pytest.mark.parametrize(
+    "test_ingredients,expected", 
+    [([],[]), # return empty list for empty ingredients
+    (['chorizo'], []), # return empty list for ridiculous ingredients
+    (['coffee', 'milk'], ['Coffee']), # Can only make coffee with coffee and milk
+    (['coffee', 'milk', 'chorizo'], ['Coffee']), # Extra ingredients don't affect results unless drink is available
+    (['coffee', 'milk', 'tea'], ['Coffee', 'Tea']), # Can make coffee and tea
+    (['coffee', 'milk', 'tea', 'sugar'], ['Coffee', 'Tea', 'Cappuccino']) # can make all test drinks
+    ]
+    )
+def test_get_drinks_from_ingredients(test_ingredients, expected):
 
-    available_ingredients = ['coffee', 'milk', 'sugar']
     drinks_database = mock_drinks_api()
 
-    drinks = get_drinks_from_ingredients(available_ingredients, drinks_database)
+    drinks = get_drinks_from_ingredients(test_ingredients, drinks_database)
 
-    assert drinks == ['Coffee', 'Cappuccino']
+    assert sorted(drinks) == sorted(expected)
+
+@pytest.fixture
+def mock_get_all_drinks_containing_ingredient(monkeypatch):
+
+    def mock_get_all_drinks_containing_ingredient_func(available_ingredient):
+        """
+        Use mock drinks api to get drinks
+        """
+        all_drinks = mock_drinks_api()
+        drinks_list = [ 
+            drink['name'] for drink in all_drinks if available_ingredient in drink['ingredients'] 
+        ]
+        return drinks_list
+
+    monkeypatch.setattr(
+        'functions.get_all_drinks_containing_ingredient',
+        mock_get_all_drinks_containing_ingredient_func,
+    )
+
+
+@pytest.mark.parametrize(
+    "test_ingredients,expected", 
+    [([],[]), # return empty list for empty ingredients
+    (['sugar'], ['Cappuccino']), # Cappuccino is the only drink which has sugar
+    (['sugar', 'tea'], ['Cappuccino', 'Tea']) # Cappuccino and Tea only drinks which contain one of these ingredients
+    ]
+    )
+def test_get_all_drinks_containing_ingredient(mock_get_all_drinks_containing_ingredient,test_ingredients,expected ):
+    """
+    Get a list of drinks which contain `available_ingredient`
+    """
+    drinks = get_possible_drinks(test_ingredients)
+
+    assert sorted(drinks) == sorted(expected)
